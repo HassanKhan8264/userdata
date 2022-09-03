7db509a7d4d67e13b7b283fd9b7f77f68d81a2c6
hassan khan <hf610061@gmail.com>
2022-09-02 19:30:10 -0700

    new

---
+++ app.js
@@ -0,0 +1,382 @@
+var usersList = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+    {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+            "street": "Douglas Extension",
+            "suite": "Suite 847",
+            "city": "McKenziehaven",
+            "zipcode": "59590-4157",
+            "geo": {
+                "lat": "-68.6102",
+                "lng": "-47.0653"
+            }
+        },
+        "phone": "1-463-123-4447",
+        "website": "ramiro.info",
+        "company": {
+            "name": "Romaguera-Jacobson",
+            "catchPhrase": "Face to face bifurcated interface",
+            "bs": "e-enable strategic applications"
+        }
+    },
+    {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "username": "Karianne",
+        "email": "Julianne.OConner@kory.org",
+        "address": {
+            "street": "Hoeger Mall",
+            "suite": "Apt. 692",
+            "city": "South Elvis",
+            "zipcode": "53919-4257",
+            "geo": {
+                "lat": "29.4572",
+                "lng": "-164.2990"
+            }
+        },
+        "phone": "493-170-9623 x156",
+        "website": "kale.biz",
+        "company": {
+            "name": "Robel-Corkery",
+            "catchPhrase": "Multi-tiered zero tolerance productivity",
+            "bs": "transition cutting-edge web services"
+        }
+    },
+    {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "username": "Kamren",
+        "email": "Lucio_Hettinger@annie.ca",
+        "address": {
+            "street": "Skiles Walks",
+            "suite": "Suite 351",
+            "city": "Roscoeview",
+            "zipcode": "33263",
+            "geo": {
+                "lat": "-31.8129",
+                "lng": "62.5342"
+            }
+        },
+        "phone": "(254)954-1289",
+        "website": "demarco.info",
+        "company": {
+            "name": "Keebler LLC",
+            "catchPhrase": "User-centric fault-tolerant solution",
+            "bs": "revolutionize end-to-end systems"
+        }
+    },
+    {
+        "id": 6,
+        "name": "Mrs. Dennis Schulist",
+        "username": "Leopoldo_Corkery",
+        "email": "Karley_Dach@jasper.info",
+        "address": {
+            "street": "Norberto Crossing",
+            "suite": "Apt. 950",
+            "city": "South Christy",
+            "zipcode": "23505-1337",
+            "geo": {
+                "lat": "-71.4197",
+                "lng": "71.7478"
+            }
+        },
+        "phone": "1-477-935-8478 x6430",
+        "website": "ola.org",
+        "company": {
+            "name": "Considine-Lockman",
+            "catchPhrase": "Synchronised bottom-line interface",
+            "bs": "e-enable innovative applications"
+        }
+    },
+    {
+        "id": 7,
+        "name": "Kurtis Weissnat",
+        "username": "Elwyn.Skiles",
+        "email": "Telly.Hoeger@billy.biz",
+        "address": {
+            "street": "Rex Trail",
+            "suite": "Suite 280",
+            "city": "Howemouth",
+            "zipcode": "58804-1099",
+            "geo": {
+                "lat": "24.8918",
+                "lng": "21.8984"
+            }
+        },
+        "phone": "210.067.6132",
+        "website": "elvis.io",
+        "company": {
+            "name": "Johns Group",
+            "catchPhrase": "Configurable multimedia task-force",
+            "bs": "generate enterprise e-tailers"
+        }
+    },
+    {
+        "id": 8,
+        "name": "Nicholas Runolfsdottir V",
+        "username": "Maxime_Nienow",
+        "email": "Sherwood@rosamond.me",
+        "address": {
+            "street": "Ellsworth Summit",
+            "suite": "Suite 729",
+            "city": "Aliyaview",
+            "zipcode": "45169",
+            "geo": {
+                "lat": "-14.3990",
+                "lng": "-120.7677"
+            }
+        },
+        "phone": "586.493.6943 x140",
+        "website": "jacynthe.com",
+        "company": {
+            "name": "Abernathy Group",
+            "catchPhrase": "Implemented secondary concept",
+            "bs": "e-enable extensible e-tailers"
+        }
+    },
+    {
+        "id": 9,
+        "name": "Glenna Reichert",
+        "username": "Delphine",
+        "email": "Chaim_McDermott@dana.io",
+        "address": {
+            "street": "Dayna Park",
+            "suite": "Suite 449",
+            "city": "Bartholomebury",
+            "zipcode": "76495-3109",
+            "geo": {
+                "lat": "24.6463",
+                "lng": "-168.8889"
+            }
+        },
+        "phone": "(775)976-6794 x41206",
+        "website": "conrad.com",
+        "company": {
+            "name": "Yost and Sons",
+            "catchPhrase": "Switchable contextually-based project",
+            "bs": "aggregate real-time technologies"
+        }
+    },
+    {
+        "id": 10,
+        "name": "Clementina DuBuque",
+        "username": "Moriah.Stanton",
+        "email": "Rey.Padberg@karina.biz",
+        "address": {
+            "street": "Kattie Turnpike",
+            "suite": "Suite 198",
+            "city": "Lebsackbury",
+            "zipcode": "31428-2261",
+            "geo": {
+                "lat": "-38.2386",
+                "lng": "57.2232"
+            }
+        },
+        "phone": "024-648-3804",
+        "website": "ambrose.net",
+        "company": {
+            "name": "Hoeger LLC",
+            "catchPhrase": "Centralized empowering task-force",
+            "bs": "target end-to-end models"
+        }
+    }
+];
+
+var divOfList = document.getElementById("userList");
+
+// filtering array of object by using (var in loop)
+for (var key in usersList) {
+
+    // if (typeof (usersList[key].address) === "object") {
+    //     console.log((usersList[key].address))
+    // }
+    // if (typeof (usersList[key].company) === "object") {
+    //     console.log((usersList[key].company))
+    // }
+    // creating main div
+    var div = document.createElement('div')
+    div.setAttribute("class", "list")
+    div.setAttribute("key", usersList[key].id)
+    // creating user id in a parent
+    var divId = document.createElement('div')
+    divId.setAttribute("class", 'left-box')
+    var spanId = document.createElement('span')
+    var spanIdText = document.createTextNode(usersList[key].id + ":")
+    // creating user name in a parent
+    var divName = document.createElement('div')
+    divName.setAttribute("class", 'center-box')
+    var spanName = document.createElement('span')
+    var spanNameText = document.createTextNode(usersList[key].name)
+    //creating a button which is getting detail of sepecific data
+    var btn = document.createElement('button')
+    btn.setAttribute("class", "details")
+    btn.setAttribute("onClick", `details(${usersList[key].id})`)
+    var btnText = document.createTextNode('Details')
+    // creating id in a DOM list
+    spanId.appendChild(spanIdText)
+    divId.appendChild(spanId)
+    div.appendChild(divId)
+    // creating name of users in a DOM list
+    spanName.appendChild(spanNameText)
+    divName.appendChild(spanName)
+    div.appendChild(divName)
+    // adding button in dom list
+    btn.appendChild(btnText)
+    div.appendChild(btn)
+    
+    
+    divOfList.appendChild(div)
+    
+    
+}
+
+var modal = document.getElementById("modal")
+var modalContent = document.getElementById("modalContent")
+
+
+function details(el) {
+    console.log(el)
+    for (var key in usersList) {
+        if (usersList[key].id === el) {
+            modal.style.display = "flex"
+            modalContent.appendChild(para)                        
+            // modalContent.style.display = "block"
+            // dataDiv.setAttribute(displayDiv())
+                //  modalContent.innerHTML = (usersList[key].name + '\n' +
+                // usersList[key].id + '\n'
+                // )
+
+            }
+        }
+}
+    
+// var closeBtn = document.getElementById("close")
+
+var clos = document.createElement("div")
+var closeText = document.createElement("i")
+closeText.setAttribute("class", "bi bi-x-lg")
+
+clos.setAttribute("class", "button") 
+clos.setAttribute("onclick", "cl()") 
+clos.appendChild(closeText)
+modalContent.appendChild(clos)
+
+
+function Data(id,name,username,email,address,street,suite,city,zipcode,geo,lat,lng,phone,website,company,companyname,catchPhrase,bs){
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.name = name;
+    this.email = email;
+    this.address = address;
+    this.street = street;
+    this.suite = suite;
+    this.city = city;
+    this.zipcode = zipcode;
+    this.geo = geo;
+    this.lat = lat;
+    this.lng = lng;
+    this.phone = phone;
+    this.website = website;
+    this.company = company;
+    this.companyname = companyname;
+    this.catchPhrase = catchPhrase;
+    this.bs = bs;
+}
+
+var user1 = new Data("1","Leanne Graham","Bret","Sincer@gmail.com",":","Kulas Light","apt . 556","Gwenboronog","909099",":","-37.332","762.2287","1-28398398-278273","hildegrad.org",":","Romaguera-Crona","Multi-layered clint-server neutral-net","harness real time e-markets")
+var user2 = new Data("2","Ervin Howell","B","Sincer@gmail.com",":","Kulas Light","apt . 556","Gwenboronog","909099",":","-37.332","762.2287","1-28398398-278273","hildegrad.org",":","Romaguera-Crona","Multi-layered clint-server neutral-net","harness real time e-markets")
+
+
+for(var key in user1){
+    var para = document.createElement("p")
+    var text = document.createTextNode(key)
+    var span = document.createElement("span")
+    var value = document.createTextNode(":"+ user1[key])
+    span.appendChild(value)
+    para.appendChild(text)
+    para.appendChild(span)
+    modalContent.appendChild(para)
+}
+
+for(var key2 in user2){
+    
+    var para2 = document.createElement("p")
+    var text2 = document.createTextNode(key2)
+    var span2 = document.createElement("span")
+    var value2 = document.createTextNode(":"+ user2[key2])
+    span2.appendChild(value2)
+    para2.appendChild(text2)
+    para2.appendChild(span2)
+    modalContent.appendChild(para2)
+}
+
+
+// modal.appendChild(modalContent)
+
+function cl(){
+      modal.style.display = "none"
+        
+}
+    
+
+
+
+
+// function modal1() {
+    //     if (modal.style.display === "none") {
+        //         modal.style.display = 'flex'
+        //     } else {
+            //         modal.style.display = 'none'
+            //     }
+            // }
+            
